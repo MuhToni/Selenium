034c5166dda889f402380d2b9a2b263ed4bd88ec
SysAdmin <cadettoni@gmail.com>
2022-07-15 14:04:51 -0400

    First Commit

---
+++ src/syntax/class05/HomeWork.java
@@ -0,0 +1,24 @@
+/*package syntax.class05;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomeWork {
+        public static String url = "https://syntaxprojects.com/basic-select-dropdown-demo.php";
+
+        public static void main (String[]args) throws InterruptedException {
+            System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+            driver.get(url);
+            driver.findElement(By.id("u_3_b_cT")).sendKeys("Good");
+            driver.findElement(By.id("u_3_d_uW")).sendKeys("Syntax");
+            driver.findElement(By.id("u_3_g_Ds")).sendKeys("Syntax00@syntax.org");
+            driver.findElement(By.id("password_step_input")).sendKeys("SyntaxBusy1");
+            driver.findElement(By.)
+
+
+
+        }
+    }
+*/
